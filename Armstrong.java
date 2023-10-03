f49c5e57a5639d28da3e6bb4ec2319cba4775086
MoolaPravalesh19 <144197134+MoolaPravalesh19@users.noreply.github.com>
2023-10-03 12:44:17 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -4,23 +4,23 @@ class Armstrong
     public static void main(String args[])
 	{
 	    Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a no.:");
-		int a=sc.nextInt();
-		int r,sum=0,temp;
-		temp=a;
-		while(a>0)
-		{
-		  r=a%10;
-	      sum=(r*r*r)+sum;
-		  a=a/10;
-		}
-		if(temp==sum)
-		{
-		    System.out.println("Armstrong no.");
-		}
-		else
-		{
-		    System.out.println("not an armstrng no.");
-		}   
+	    System.out.println("Enter a no.:");
+	    int a=sc.nextInt();
+	    int r,sum=0,temp;
+	    temp=a;
+	    while(a>0)
+	    {
+		r=a%10;
+	        sum=(r*r*r)+sum;
+	        a=a/10;
+	    }
+	    if(temp==sum)
+	    {
+		System.out.println("Armstrong no.");
+	    }
+	    else
+	    {
+		System.out.println("not an armstrng no.");
+	    }   
 	}
 }
