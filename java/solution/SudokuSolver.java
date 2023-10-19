7d62150f6286e0705692cc29bf7aded1ec25e903
Sumitha Nair <61596065+sumitha1998@users.noreply.github.com>
2023-10-19 19:21:51 +0530

    Create SudokuSolver.java

---
+++ java/solution/SudokuSolver.java
@@ -0,0 +1,86 @@
+// This program uses a backtracking algorithm to solve the Sudoku puzzle.
+public class SudokuSolver {
+    public static void main(String[] args) {
+        int[][] sudokuBoard = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(sudokuBoard)) {
+            printSudoku(sudokuBoard);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveSudoku(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = 0; // backtrack
+                            }
+                        }
+                    }
+                    return false; // no valid number can be placed, need to backtrack
+                }
+            }
+        }
+        return true; // all cells are filled
+    }
+
+    public static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check if the number is not present in the current row, column, and the 3x3 grid
+        return !isInRow(board, row, num) && !isInColumn(board, col, num) && !isInBox(board, row - row % 3, col - col % 3, num);
+    }
+
+    public static boolean isInRow(int[][] board, int row, int num) {
+        for (int col = 0; col < 9; col++) {
+            if (board[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isInColumn(int[][] board, int col, int num) {
+        for (int row = 0; row < 9; row++) {
+            if (board[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isInBox(int[][] board, int startRow, int startCol, int num) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row + startRow][col + startCol] == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void printSudoku(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
