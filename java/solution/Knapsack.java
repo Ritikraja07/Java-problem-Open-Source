2d030c7313fd0289a3f5d68691e273b71efe069b
Sumitha Nair <61596065+sumitha1998@users.noreply.github.com>
2023-10-29 09:04:47 +0530

    0/1 Knapsack Problem in Java using dynamic programming

---
+++ java/solution/Knapsack.java
@@ -0,0 +1,32 @@
+/* The time complexity of this dynamic programming solution is O(n * capacity), 
+ * where n is the number of items and capacity is the capacity of the knapsack.
+ */
+
+public class Knapsack {
+
+    public static int knapsack(int[] weights, int[] values, int capacity) {
+        int n = weights.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= capacity; w++) {
+                if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(dp[i - 1][w], values[i - 1] + dp[i - 1][w - weights[i - 1]]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[n][capacity];
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {2, 3, 4, 5};
+        int[] values = {3, 4, 5, 6};
+        int capacity = 5;
+
+        int maxValue = knapsack(weights, values, capacity);
+        System.out.println("Maximum value that can be obtained: " + maxValue);
+    }
+}
